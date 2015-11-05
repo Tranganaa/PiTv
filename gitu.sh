9a9ef1063d4c27864923fc0f0820ae1ae53baa72
Sathiyajeyan <srangana@uoguelph.ca>
2015-11-05 11:17:38 -0500

    shit

--- gitu.sh
+++ gitu.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 git add .
-git 
+while getopts ":m:" opt; do
+  case $opt in
+    m)
+      git commit -m "$OPTARG">&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
